b80b8e6b706e024d0f8cd807c5bddc2be5ab2df1
Sandeep Singh Mehra <sandysmart017@gmail.com>
2023-07-02 17:04:35 +0530

    Added pages

--- src/app/page.client.js
+++ src/app/page.client.js
@@ -1,8 +1,40 @@
 'use client'
 import React from 'react';
-import { FaUser, FaBell } from 'react-icons/fa';
-import { BiSolidRightArrow } from 'react-icons/bi';
-
+import { AiOutlineEye } from 'react-icons/ai';
+import { FaUser, FaBell, FaRegHeart, FaHeart } from 'react-icons/fa';
+import { BiSolidRightArrow, BiSolidMessageAltDetail, BiShareAlt, BiComment, BiSolidDollarCircle } from 'react-icons/bi';
+import { FiSearch } from 'react-icons/fi';
+import { FcBusinessman, FcBusinesswoman } from 'react-icons/fc';
+import Image from 'next/image';  
+import Sidebar from '@/components/Sidebar';
+const market_stories = [
+  {
+    image_url: '/images/Borough-Market.webp',
+    alt: 'Borough-Market',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },{
+    image_url: '/images/mumbai-maharastraindia.jpeg',
+    alt: 'Mumbai-Maharastraindia',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+  },{
+    image_url: '/images/Borough-Market.webp',
+    alt: 'Borough-Market',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },{
+    image_url: '/images/mumbai-maharastraindia.jpeg',
+    alt: 'Mumbai-Maharastraindia',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },
+  {
+    image_url: '/images/Borough-Market.webp',
+    alt: 'Borough-Market',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },{
+    image_url: '/images/mumbai-maharastraindia.jpeg',
+    alt: 'Mumbai-Maharastraindia',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },
+]
 function PageClientComponent() {
   const [isSidebarVisible, setSidebarVisible] = React.useState(true);
 
@@ -10,58 +42,168 @@ function PageClientComponent() {
     setSidebarVisible(!isSidebarVisible);
   };
   return (
-    <div className={`flex-1 relative h-full flex bg-white`}>
+    <div className={`flex-1 relative height-main-section flex bg-white`}>
         {isSidebarVisible && (
-        <aside className='bg-blue-900 w-40 h-full flex-shrink-0 transition-all duration-300'>
-          {/* <div className="flex flex-col h-full"> */}
-          <ul>
-            <li className='relative'>
-              <div>
-                <FaUser size={20} color='white' className='inline-block' />
-                <span className=''>Hello, User</span>
-                <FaBell size={20} color='white' className='inline-block absolute right-0' />
-              </div>
-            </li>
-            <li>
-              <span>Discussion Fourm</span>
-            </li>
-            <li>
-              <span>Market Stories</span>
-            </li>
-            <li>
-              Sentiment
-            </li>
-            <li>
-              Market
-            </li>
-            <li>
-              Sector
-            </li>
-            <li>
-              Watchlist
-            </li>
-            <li>
-              Events
-            </li>
-            <li>
-              News/Interview
-            </li>
-          </ul>
-          {/* </div> */}
+        <aside className='bg-blue-900 w-60 h-full flex-shrink-0 transition-all duration-300'>
+          <Sidebar />
         </aside>
         )}
-        <section className={`flex-1 w-full h-full relative ${
-          isSidebarVisible ? 'ml-1' : 'ml-0'
-        } bg-white transition-all duration-300`}>
+        <section className={`flex-1 w-full h-full relative ${isSidebarVisible ? 'ml-0' : 'ml-0'} bg-white transition-all duration-300`}>
             <button onClick={toggleSidebar} className='absolute top-64 origin-center h-20 bg-blue-900'>
                 <BiSolidRightArrow size={20} color={'white'} />
             </button>
-            <div className="h-full flex flex-row">
-              <div className={`flex-grow bg-red-500 ${isSidebarVisible ? 'w-3/4' : 'w-full'}`}>
-                Section1
+            <div className="h-full flex flex-row bg-gray-100">
+              <div className={`h-full ${isSidebarVisible ? 'w-2/3' : 'w-full'} p-5`}>
+                <h1 className='uppercase text-xl font-bold text-red-400 h-8'>Discussion Fourm</h1>
+                <div className='pl-10 pr-5 h-full'>
+                  <label className='h-10 text-xl font-semibold'>Filters</label>
+                  <div className='bg-white flex h-18 justify-evenly py-5 rounded-lg'>
+                      <div className='w-fit h-8 px-3 py-1 bg-red-700 text-white border border-solid rounded-full'>Sector 1</div>
+                      <div className='w-fit h-8 px-3 py-1 bg-blue-800 text-white border border-solid rounded-full'>Sector 2</div>
+                      <div className='w-fit h-8 px-3 py-1 bg-yellow-400 text-white border border-solid rounded-full'>Sector 3</div>
+                      <div className='w-fit h-8 bg-gray-300 flex justify-end items-center border border-solid border-gray-300 rounded-full'>
+                        <FiSearch size={20} color='black' className='inline-block' />
+                        <input type={'text'} placeholder='Search here' className='bg-inherit rounded-r-full pl-1 focus:outline-none'/>
+                      </div>
+                  </div>
+                  <div className='discussion-section-height overflow-auto'>
+                    <div className='bg-white flex justify-evenly my-5 rounded-lg py-5 px-10'>
+                      <div className='w-full m-auto'>
+                        <p className='text-blue-400 float-right text-xs'>2 min ago</p>
+                        <div className="relative mx-auto w-fit h-fit flex place-items-center rounded-lg">
+                          <Image
+                            className="relative object-cover w-full h-44 rounded-lg"
+                            src={'/images/infibeam-logo.png'}
+                            alt={'infibeam'}
+                            width={180}
+                            height={30}
+                            priority
+                          />
+                        </div>
+                        <div className='flex'>
+                          <FcBusinessman size={40} className='bg-red-950 rounded-full' />
+                          <div>
+                            <span><strong>Lorum Ipsum</strong></span>
+                            <span className='w-fit h-8 px-1 py-1 bg-red-700 text-white font-light border border-solid rounded-full'>Sector 1</span>
+                            <div>
+                              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industy. Lorem Ipsum is</p>
+                              <div className='flex justify-evenly w-full'>
+                                <div className='flex flex-row items-center'>
+                                  <FaHeart color='red'/>
+                                  <p>2k</p>
+                                </div>
+                                <div className='flex flex-row items-center'>
+                                  <AiOutlineEye />
+                                  <p>2k</p>
+                                </div>
+                                <div className='flex flex-row items-center'>
+                                  <BiComment />
+                                  <p>2k comments</p>
+                                </div>
+                                <div className='flex flex-row items-center'>
+                                  <BiShareAlt />
+                                  <p>share</p>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        
+                      </div>
+                        
+                    </div>
+                    <div className='bg-white flex justify-evenly my-5 rounded-lg py-5 px-10'>
+                      <div className='w-full m-auto'>
+                        <p className='text-blue-400 float-right text-xs'>2 min ago</p>
+                        <div className='flex'>
+                          <FcBusinessman size={40} className='bg-red-950 rounded-full' />
+                          <div>
+                            <span><strong>Lorum Ipsum</strong></span>
+                            <span className='w-fit h-8 px-1 py-1 bg-red-700 text-white font-light border border-solid rounded-full'>Sector 1</span>
+                            <div>
+                              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industy. Lorem Ipsum is</p>
+                              <div className='flex justify-evenly w-full'>
+                                <div className='flex flex-row items-center'>
+                                  <FaRegHeart color='red'/>
+                                  <p>2k</p>
+                                </div>
+                                <div className='flex flex-row items-center'>
+                                  <AiOutlineEye />
+                                  <p>2k</p>
+                                </div>
+                                <div className='flex flex-row items-center'>
+                                  <BiComment />
+                                  <p>2k comments</p>
+                                </div>
+                                <div className='flex flex-row items-center'>
+                                  <BiShareAlt />
+                                  <p>share</p>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className='bg-white flex justify-evenly my-5 rounded-lg py-5 px-10'>
+                      <div className='w-full m-auto'>
+                        <p className='text-blue-400 float-right text-xs'>2 min ago</p>
+                        <div className='flex'>
+                          <FcBusinesswoman size={40} className='bg-red-950 rounded-full' />
+                          <div>
+                            <span><strong>Lorum Ipsum</strong></span>
+                            <span className='w-fit h-8 px-1 py-1 bg-red-700 text-white font-light border border-solid rounded-full'>Sector 1</span>
+                            <div>
+                              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industy. Lorem Ipsum is</p>
+                              <div className='flex justify-evenly w-full'>
+                                <div className='flex flex-row items-center'>
+                                  <FaHeart color='red'/>
+                                  <p>2k</p>
+                                </div>
+                                <div className='flex flex-row items-center'>
+                                  <AiOutlineEye />
+                                  <p>2k</p>
+                                </div>
+                                <div className='flex flex-row items-center'>
+                                  <BiComment />
+                                  <p>2k comments</p>
+                                </div>
+                                <div className='flex flex-row items-center'>
+                                  <BiShareAlt />
+                                  <p>share</p>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        
+                      </div>
+                        
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className={`flex-grow bg-blue-500 ${isSidebarVisible ? 'w-1/4' : 'w-full'}`}>
-                Section2
+              <div className={`h-full ${isSidebarVisible ? 'w-1/3' : 'w-full'} p-5`}>
+              <h1 className='uppercase text-xl font-bold text-red-400 bg-gray-100 h-8 pl-5'>Market Stories</h1>
+              <div className='flex flex-wrap justify-between custom-height-market-story gap-10 overflow-auto p-5'>
+                {market_stories.map((story,i)=>(
+                <div key={i} className="relative w-fit h-fit flex place-items-center rounded-lg">
+                  <Image
+                    className="relative object-cover w-80 h-44 rounded-lg"
+                    src={story.image_url}
+                    alt={story.alt}
+                    width={180}
+                    height={30}
+                    priority
+                  />
+                  <div className="absolute top-0 left-0 w-full h-full flex items-end justify-center rounded-lg">
+                    <h2 className="text-white line-clamp-3 text-sm h-20 font-light bg-black bg-opacity-50 p-4 rounded-lg">
+                      {story.text}
+                    </h2>
+                  </div>
+                </div>))}
+
+              </div>
               </div>
             </div>
         </section>
--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,73 @@
-import Image from 'next/image'
+import Image from 'next/image';
 import { AiOutlinePlus } from 'react-icons/ai';
-import { FiSearch } from 'react-icons/fi';
 import PageClientComponent from './page.client';
+import Navbar from '@/components/Navbar';
+import MarqueeHeader from '@/components/MarqueeHeader';
+
+const Companies_info = [
+  {
+    image_url: '/images/capri.jpg',
+    alt_text: 'capri'
+  },{
+    image_url: '/images/ganeshhousing.jpg',
+    alt_text: 'ganeshhousing'
+  },{
+    image_url: '/images/fino.png',
+    alt_text: 'fino'
+  },{
+    image_url: '/images/gravita.jpg',
+    alt_text: 'gravita'
+  },{
+    image_url: '/images/godawari.jpg',
+    alt_text: 'godawari'
+  },{
+    image_url: '/images/deepind.png',
+    alt_text: 'deepind'
+  },{
+    image_url: '/images/dtpatten1.png',
+    alt_text: 'dtpatten1'
+  },{
+    image_url: '/images/capri.jpg',
+    alt_text: 'capri'
+  },{
+    image_url: '/images/ganeshhousing.jpg',
+    alt_text: 'ganeshhousing'
+  },{
+    image_url: '/images/fino.png',
+    alt_text: 'fino'
+  },{
+    image_url: '/images/gravita.jpg',
+    alt_text: 'gravita'
+  },{
+    image_url: '/images/godawari.jpg',
+    alt_text: 'godawari'
+  }
+]
 
 export default function Home() {
   
   return (
-    <main className="relative h-screen overflow-hidden p-0">
-      <div className="flex flex-col h-full">
-        <nav className={`flex items-center justify-between w-full h-16 bg-white`}>
-          <p>Logo</p>
-          <div className='w-96 h-8 bg-gray-300 flex justify-end items-center border border-solid border-gray-500 rounded'>
-            <FiSearch size={20} color='black' className='inline-block mr-2' />
-          </div>
-          <div className='space-x-4'>
-            <span className='pl-4 text-gray-500'>Contact Us</span>
-            <button className='border text-gray-500 px-3 py-1 border-solid border-gray-500 rounded'>SIGN UP</button>
-            <button className='border text-gray-500 px-3 py-1 border-solid border-gray-500 rounded'>SIGN IN</button>
-          </div>
-        </nav>
-        <marquee className={'bg-black text-white h-6'} behavior="scroll" direction="right" scrollamount="1">Slow Scrolling</marquee>
-        <p className='capitalize text-black h-8'>Featured Companies</p>
-        <div className='h-14 bg-cyan-100'></div>
+    <main className="relative w-full h-screen overflow-hidden p-0">
+        <Navbar />
+        <MarqueeHeader />
+        <p className='uppercase text-sm lg:text-xl font-bold text-red-400 bg-white h-8 pl-5 flex items-center'>Featured Companies</p>
+        <div className="overflow-x-auto snap snap-x snap-mandatory flex relative bg-cyan-50 px-3">
+          {Companies_info.map((company, i)=>(
+            <Image
+              key={i}
+              className="relative snap-start object-cover w-14 h-14 lg:w-40 lg:h-14 border lg:border-none border-solid border-red-500 rounded-full lg:rounded-none px-1 mx-2 lg:px-3"
+              src={company.image_url}
+              alt={company.alt_text}
+              width={110}
+              height={11}
+              priority
+            />
+          ))}
+        </div>
         <PageClientComponent />
         <div className='absolute bottom-10 right-10 bg-blue-900 rounded-full p-3'>
           <AiOutlinePlus size={40} color='white' />
         </div>
-      </div>
     </main>
   )
 }
